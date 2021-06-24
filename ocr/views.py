e17e56db2424aa7ef181207080796db90f9b6d90
truonghoangthuan <tthun32@gmail.com>
2021-06-24 21:50:15 +0700

    24/06/2021 21h50

--- ocr/views.py
+++ ocr/views.py
@@ -6,39 +6,97 @@ from .main import *
 
 
 # Create your views here.
+# View to handle upload id card images.
 def base(request):
     text = ""
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ImagesForm(request.POST, request.FILES)
         if form.is_valid():
-            save_image = Images.objects.create(image=form.cleaned_data.get('image'))
+            save_image = Images.objects.create(image=form.cleaned_data.get("image"))
 
             # Get upload image name
-            image = request.FILES['image']
+            image = request.FILES["image"]
             # Get upload image path
-            path = settings.MEDIA_ROOT + '\images\\' + image.name
+            path = settings.MEDIA_ROOT + "\images\\" + image.name
             # Pass upload image path to ocr function
             text = text + str(extract(path))
-            print('text: ' + text)
+
+            print("text: \n" + text)
             save_image.result = text
             save_image.save()
 
-            return redirect('home')
+            return redirect("home")
 
     form = ImagesForm()
     images = Images.objects.all()
 
     context = {
-        'form': form,
-        'images': images,
+        "form": form,
+        "images": images,
     }
 
-    return render(request, 'CMND.html', context)
+    return render(request, "CMND.html", context)
 
 
+# View to handle student card upload images.
 def student_card_page(request):
-    return render(request, 'TSV.html')
+    text = ""
+    if request.method == "POST":
+        form = ImagesForm(request.POST, request.FILES)
+        if form.is_valid():
+            save_image = Images.objects.create(image=form.cleaned_data.get("image"))
+
+            # Get upload image name
+            image = request.FILES["image"]
+            # Get upload image path
+            path = settings.MEDIA_ROOT + "\images\\" + image.name
+            # Pass upload image path to ocr function
+            text = text + str(extract(path))
+
+            print("text: \n" + text)
+            save_image.result = text
+            save_image.save()
+
+            return redirect("home")
+
+    form = ImagesForm()
+    images = Images.objects.all()
+
+    context = {
+        "form": form,
+        "images": images,
+    }
+
+    return render(request, "TSV.html", context)
 
 
+# View to handle driving license upload images.
 def driving_license_page(request):
-    return render(request, 'GPLX.html')
+    text = ""
+    if request.method == "POST":
+        form = ImagesForm(request.POST, request.FILES)
+        if form.is_valid():
+            save_image = Images.objects.create(image=form.cleaned_data.get("image"))
+
+            # Get upload image name
+            image = request.FILES["image"]
+            # Get upload image path
+            path = settings.MEDIA_ROOT + "\images\\" + image.name
+            # Pass upload image path to ocr function
+            text = text + str(extract(path))
+
+            print("text: \n" + text)
+            save_image.result = text
+            save_image.save()
+
+            return redirect("home")
+
+    form = ImagesForm()
+    images = Images.objects.all()
+
+    context = {
+        "form": form,
+        "images": images,
+    }
+
+    return render(request, "GPLX.html", context)
