773f359e1e3ecafdf1e5fcb5232321e8229b3332
truonghoangthuan <tthun32@gmail.com>
2021-07-15 17:14:40 +0700

    15/07/2021 17h15

--- django_ocr_app/settings.py
+++ django_ocr_app/settings.py
@@ -118,6 +118,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
