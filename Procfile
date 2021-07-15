e382e68e64f524f5b524be7a50d1a8d802fa9029
truonghoangthuan <tthun32@gmail.com>
2021-07-15 16:55:03 +0700

    15/07/2021 16h55

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn django_ocr_app.wsgi --log-file -
+web: gunicorn django_ocr_app.wsgi
--- django_ocr_app/settings.py
+++ django_ocr_app/settings.py
@@ -128,11 +128,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
