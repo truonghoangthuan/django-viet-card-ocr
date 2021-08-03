de178f11d2f13b26e10b3540879dc52eb442cb8a
truonghoangthuan <tthun32@gmail.com>
2021-08-03 13:44:37 +0700

    03/08/2021 13h45

--- django_ocr_app/settings.py
+++ django_ocr_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e&7k+de7=e)r!-yc+-he7)=kgn9!@mxtt!xitpf6gqtny@18ix
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
